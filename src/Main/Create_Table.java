2de5cd6a7c76dcba20ec10b905ce9546603af8fb
jcast6 <89822103+jcast6@users.noreply.github.com>
2021-10-13 10:56:27 -0700

    Added a block to insert data.

--- src/Main/Create_Table.java
+++ src/Main/Create_Table.java
@@ -3,20 +3,19 @@ package Main;
 import java.sql.*;
 
 public class Create_Table {
-
     // These are needed to connect to database.
     static String url = "jdbc:mysql://localhost: 3306/getting_data";
     static final String username = "root";
     static final String password = "peter";
-
     static Statement st = null;
     static Statement st1 = null;
     static Statement st2 = null;
     static Connection con = null;
     static String sql = null;
+    static String insert_data = null;
+    static String drop_table_sql;
     static String show_table = null;
     static ResultSet rs = null;
-
 
     public static void main(String[] args) {
 
@@ -30,21 +29,18 @@ public class Create_Table {
             System.out.println("Cannot connect to database.");
             e.printStackTrace();
         }
-/*
-        try( Statement st = con.createStatement();){
+
+        try( Statement st = con.createStatement()){
             drop_table_sql = "DROP TABLE IF EXISTS DEPARTMENT";
             st.executeUpdate(drop_table_sql);
             System.out.println("Deleting table...");
             System.out.println("Table deleted.");
-
         }catch(SQLException e) {
             e.printStackTrace();
         }
 
- */
-
         //Using try-with-resources
-        try(Statement st1 = con.createStatement()) {
+        try(Statement st = con.createStatement()) {
             sql = "CREATE TABLE DEPARTMENT " +
                     "(dept_id INTEGER not NULL, " +
                     " dept_name VARCHAR(255), " +
@@ -53,7 +49,7 @@ public class Create_Table {
 
             System.out.println("Creating table...");
 
-            st1.executeUpdate(sql);
+            st.executeUpdate(sql);
             System.out.println("Table created!");
         } catch(SQLException e1) {
             System.out.println("Table not created.");
@@ -61,7 +57,15 @@ public class Create_Table {
 
         }
 
+        try(Statement st1 = con.createStatement()) {
+            insert_data = "INSERT INTO DEPARTMENT(dept_id, dept_name, dept_manager) VALUES(1, 'Game Design', 'Peter Green')";
 
+            st1.executeUpdate(insert_data);
+
+        }catch(SQLException e) {
+            System.out.println("Data not inserted!");
+            e.printStackTrace();
+        }
 
         try(Statement st2 = con.createStatement()) {
             show_table = "SELECT * FROM DEPARTMENT;";
@@ -79,6 +83,16 @@ public class Create_Table {
             e1.printStackTrace();
 
         }finally {
+            //For selecting the data block
+            if (st2 != null) {
+                try {
+                    st2.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            //Inserting the data block.
             if (st1 != null) {
                 try {
                     st1.close();
@@ -87,6 +101,16 @@ public class Create_Table {
                 }
             }
 
+            //Create table block
+            if(st != null) {
+                try {
+                    st.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            //For Delete if exists block
             if(st != null) {
                 try {
                     System.out.println("Connection closed!");
@@ -96,6 +120,7 @@ public class Create_Table {
                 }
             }
 
+            //For connection block.
             if (con != null) {
                 try {
                     System.out.println("Connection closing...");
