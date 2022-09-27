02503031a07bbaf988ac72cd2231fc84dd65fdd5
jcast6 <89822103+jcast6@users.noreply.github.com>
2022-09-26 22:58:18 -0700

    In the getting_data database, passwords were saved using MD5, this was to better protect the app from SQL injection. But we are trying to find a better way to secure passwords and use them in the app in a more secure way. We are also trying to find a solution to change getText() from line 89 when getting the password from the database.

--- src/Main/Db_Sign_In.java
+++ src/Main/Db_Sign_In.java
@@ -1,13 +1,18 @@
 package Main;
+/*This application is created to practice java and MySql, and develop knowledge of java
+  and MySql.
+
+  This is the first page of a store inventory page. The employee has to login by
+  their employee email and password. This is followed by the Db_Home_Page.java.
+ */
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.nio.charset.StandardCharsets;
 import java.sql.*;
 
-public class Db_Sign_In extends JFrame {
+public class Db_Sign_In extends JFrame{
 
     // These are needed to connect to database.
     String url = "jdbc:mysql://localhost: 3306/getting_data";
@@ -15,22 +20,19 @@ public class Db_Sign_In extends JFrame {
     String password = "peter";
     Connection con;
 
-    private static final long serVersUID= 1L;
-    private JTextField textF;
-    private JPasswordField pwField;
-    private JButton loginButton;
-    private JLabel label;
-    private JPanel contentPane;
+    private final JTextField textF;
+    private final JPasswordField pwField;
+    private final JButton loginButton;
+    JLabel label;
+    JPanel contentPane; //cannot be final
 
     public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    Db_Sign_In frame = new Db_Sign_In();
-                    frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+        EventQueue.invokeLater(() -> {
+            try {
+                Db_Sign_In frame = new Db_Sign_In();
+                frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         });
     }
@@ -82,40 +84,49 @@ public class Db_Sign_In extends JFrame {
         //The login button
         loginButton = new JButton("Login");
         loginButton.setBounds(550, 395, 165, 55);
-        loginButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String email = textF.getText();
-                String pw = pwField.getText();
+        loginButton.addActionListener(e -> {
+            String email = textF.getText();
+            String pw = pwField.getText();
 
-                // Try{}catch{} is used to connect to the database ,'getting_data', through a localhost and
-                // retrieve data from a table.
-                try {
 
-                    //Opening a JDBC connection.
-                    con = DriverManager.getConnection(url, username, password);
+            // Try{}catch{} is used to connect to the database ,'getting_data', through a localhost and
+            // retrieve data from a table.
+            try {
 
-                    //Getting the login information.
-                    PreparedStatement pst = (PreparedStatement) con.prepareStatement("Select emp_email, emp_password from emp_login where emp_email=? and emp_password=?");
+                //Opening a JDBC connection.
+                con = DriverManager.getConnection(url, username, password);
 
-                    pst.setString(1, email);
-                    pst.setString(2, pw);
-                    ResultSet rs = pst.executeQuery();
+                //Getting the login information.
+                //Used MD5(?) to get hashed password
+                // emp_login table
+                //+------------+------------+------------------+----------------------------------+
+                //| emp_f_name | emp_l_name | emp_email        | emp_password                     |
+                //+------------+------------+------------------+----------------------------------+
+                //| my         | love       | jazmin           | ...                           |
+                //| Juan       | Castaneda  | juan@company.com | ...                         |
+                //| paul       | fish       | paul@co.com      | ba6668af84b8057140fdedfddbac8a38 |
+                //+------------+------------+------------------+----------------------------------+
 
-                    if(rs.next()) {
-                        dispose();
-                        Db_Home_Page newUser =  new Db_Home_Page(email);
-                        newUser.setTitle("Database Homepage!");
-                        newUser.setVisible(true);
-                        JOptionPane.showMessageDialog(loginButton, "Login successful!");
+                PreparedStatement pst = con.prepareStatement("Select emp_email, emp_password from emp_login where emp_email=? and emp_password= MD5(?)");
 
-                    } else {
-                        JOptionPane.showMessageDialog(loginButton, "Login not successful!");
-                    }
-                } catch (SQLException er) {
-                    er.printStackTrace();
+                pst.setString(1, email);
+                pst.setString(2, pw);
+                ResultSet rs = pst.executeQuery();
 
+                if (rs.next()) {
+                    dispose();
+                    Db_Home_Page newUser = new Db_Home_Page(email);
+                    newUser.setTitle("Database Homepage!");
+                    newUser.setVisible(true);
+                    JOptionPane.showMessageDialog(loginButton, "Login successful!");
+
+                } else {
+                    JOptionPane.showMessageDialog(loginButton, "Login not successful! " +
+                            "\nCheck email or password!");
                 }
+            } catch (SQLException er) {
+                er.printStackTrace();
+
             }
         });
 
@@ -125,4 +136,6 @@ public class Db_Sign_In extends JFrame {
         label.setBounds(0, 0, 1008, 562);
         contentPane.add(label);
     }
+
+
 }
