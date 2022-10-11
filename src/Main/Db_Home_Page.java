568539a117a91ba68e76c6501333cf61a6d36feb
jcast6 <89822103+jcast6@users.noreply.github.com>
2022-10-10 23:36:32 -0700

    The biggest change for Db_Home_Page, was that the following code block was removed:  public static void main(String[] args) {         EventQueue.invokeLater(() -> {             try {                 Db_Sign_In frame = new Db_Sign_In();                 frame.setVisible(true);             } catch (Exception e) {                 e.printStackTrace();             }         });     } I tried to see what the use of this was but there was no change in the program functionality after removing the code block.
    The buttons were updated to different locations in the frame, and I changed the buttons to only be "Yes" or "No" buttons instead of "Yes", "No", "Cancel buttons".
    
    I am adding a new button to display an employee directory. The table and data still need to be added and created in the database.

--- src/Main/Db_Home_Page.java
+++ src/Main/Db_Home_Page.java
@@ -3,40 +3,25 @@ package Main;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.print.attribute.standard.Media;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
-
 public class Db_Home_Page extends JFrame {
 
-
-    private static final long serVersUID = 1L;
-    private JPanel contentPane;
+    JPanel contentPane;
     JButton logoutButton;
-    JFrame frame;
     JButton stockButton;
-
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    Db_Home_Page frame = new Db_Home_Page();
-                    frame.setVisible(true);
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
-
-            }
-        });
-    }
+    JButton emp_directory; //new button
 
     public Db_Home_Page() {
 
     }
 
+
     //The frame for the home page.
     public Db_Home_Page(String user) {
-        setBounds(500, 200,1015, 600);
+       setSize(1000,1000);
         setResizable(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -48,72 +33,89 @@ public class Db_Home_Page extends JFrame {
         //the company stock page
         stockButton = new JButton("Check Stock");
         stockButton.setBackground(UIManager.getColor("Button.disabledForeground"));
-        stockButton.setForeground(new Color(0, 0, 0));
-        stockButton.setFont(new Font("Times New Roman", Font.ITALIC, 40));
+        stockButton.setFont(new Font("Times New Roman", Font.BOLD, 30));
+        stockButton.setBounds(75, 100, 250, 95);
+        //stockButton.setBackground(Color.GREEN); //Can change color if needed
         stockButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                int b = JOptionPane.showConfirmDialog(stockButton, "Click to open stock page.", "Check Stock?", JOptionPane.YES_NO_OPTION);
+                int b = JOptionPane.showConfirmDialog(stockButton, "Click to yes open stock page.", "Check Stock?", JOptionPane.YES_NO_OPTION);
 
                 if (b == JOptionPane.YES_OPTION) {
                     dispose();
-                    //only to check if working
+                    //only to check if working, prints in terminal
                     System.out.println("Options are 'Yes' = 0, 'No' = 1,");
                     System.out.println("Values Clicked: " + b);
-                    System.out.println("Stock not viewed.");
+                    System.out.println("Stock viewed.");
 
-                    StockPage newStockPageView = new StockPage();
+                    StockPage newStockPageView = new StockPage(user);
                     newStockPageView.setTitle("Stock Page");
                     newStockPageView.setVisible(true);
                     JOptionPane.showMessageDialog(stockButton, "Welcome to the stock page!!");
 
-                }
-
-                else if (b == JOptionPane.NO_OPTION) {
+                } else if (b == JOptionPane.NO_OPTION) {
 
                     //only to check if working
                     System.out.println("Options are 'Yes' = 0, 'No' = 1");
                     System.out.println("Values Clicked: " + b);
-                    System.out.println("Checking stock...");
-                    frame.setVisible(true);
-                    return;
-
+                    System.out.println("Stock not checked...");
+                    JOptionPane.showMessageDialog(stockButton, "Select an option");
+                    //return;
                 }
             }
         });
 
-        stockButton.setBounds(250, 120, 490, 115);
-        stockButton.setBackground(Color.GREEN);
-        stockButton.setFont(new Font("Times New Roman", Font.ITALIC, 40));
         contentPane.add(stockButton);
 
         //The logout button.
         logoutButton = new JButton("Logout");
-        logoutButton.setBackground(Color.GREEN);
-        logoutButton.setForeground(new Color(0, 0, 0));
-        logoutButton.setFont(new Font("Times New Roman", Font.ITALIC, 40));
+        //logoutButton.setBackground(Color.GREEN); //can change color if needed
+        logoutButton.setBounds(75, 600, 250, 95);
+        logoutButton.setFont(new Font("Times New Roman", Font.BOLD, 30));
         logoutButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
 
-                int a = JOptionPane.showConfirmDialog(logoutButton, "Are you sure you want to logout?");
+                int a = JOptionPane.showConfirmDialog(logoutButton, "Are you sure you want to logout?", "Confirm logout", JOptionPane.YES_NO_OPTION);
 
                 if (a == JOptionPane.YES_OPTION) {
                     dispose();
                     //only to check if working
-                    System.out.println("Options are 'Yes' = 0, 'No' = 1, 'Cancel' = 2");
+                    System.out.println("Options are 'Yes' = 0, 'No' = 1");
                     System.out.println("Values Clicked: " + a);
                     System.out.println("GoodBye.");
 
-                } else if(a == JOptionPane.NO_OPTION) {
-                    JOptionPane.showMessageDialog(stockButton, "Login not successful!");
-
+                } else if (a == JOptionPane.NO_OPTION) {
+                    JOptionPane.showMessageDialog(logoutButton, "Glad you stayed!");
                 }
-                    System.exit(0);
             }
         });
 
-        logoutButton.setBounds(250, 320, 490, 115);
-        logoutButton.setFont(new Font("Times New Roman", Font.ITALIC, 40));
         contentPane.add(logoutButton);
+
+        //The emp_directory button
+        emp_directory = new JButton("Employee Directory");
+        //emp_directory.setBackground(Color.GREEN); //can change color if needed
+        emp_directory.setBounds(75, 200, 250, 95);
+        emp_directory.setFont(new Font("Times New Roman", Font.BOLD, 30));
+        emp_directory.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                int c = JOptionPane.showConfirmDialog(emp_directory, "Do you want to see Employee Directory?", "Confirmation window",JOptionPane.YES_NO_OPTION);
+
+                if (c == JOptionPane.YES_OPTION) {
+
+                    //only to check if working
+                    System.out.println("Options are 'Yes' = 0, 'No' = 1");
+                    System.out.println("Values Clicked: " + c);
+                    System.out.println("Opening Directory.");
+
+                } else if (c == JOptionPane.NO_OPTION) {
+                    JOptionPane.showMessageDialog(emp_directory, "Select an option");
+
+                }
+            }
+        });
+
+        contentPane.add(emp_directory);
 
     }
 }
